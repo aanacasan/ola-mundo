973d976eaae2bdc1c9a79a230970e3005d6ebc59
aanacasan <aanacasan@outlook.com>
2024-12-07 17:38:24 -0300

    primeiro commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/inicio/SobreMim";
+import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import Post from "paginas/Post";
+import ScrollToTop from "componentes/ScrollToTop";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+    <ScrollToTop />
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />          
+        </Route>
+        
+        <Route path="posts/:id/*" element={<Post />} />             
+        <Route path="*" element={<NaoEncontrada />} />        
+      </Routes>
+
+      <Rodape />    
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
